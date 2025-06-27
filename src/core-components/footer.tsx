7b3c781b7d306f6b793bcb0e3328b2ffe6134bba
Mario Silva <mbm3d2000@yahoo.com.br>
2025-06-27 18:44:09 -0300

    Montado layout

---
+++ src/core-components/footer.tsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router";
+import Text from "../components/text";
+
+
+export default function Footer() {
+    return (
+        <>
+            |<footer className="my-5 md:mt-8">
+                <nav className="flex items-center justify-center gap-4">
+                    <NavLink to="/">
+                        <Text variant="body-sm-bold" className="text-gray-300">
+                            Tarefas
+                        </Text>
+                    </NavLink>
+                    <NavLink to="/componentes">
+                        <Text variant="body-sm-bold" className="text-gray-300">
+                            Components
+                        </Text>
+                    </NavLink>
+                </nav>
+            </footer>        
+        </>
+    )
+}
